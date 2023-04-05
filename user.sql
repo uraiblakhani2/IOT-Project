df521a33ed4e8f9cd7ef912eedc21183c53bda10
uraiblakhani2 <82686794+uraiblakhani2@users.noreply.github.com>
2023-04-04 20:20:45 -0400

    Added picture in db
    Added picture in db

--- user.sql
+++ user.sql
@@ -1,10 +1,11 @@
 BEGIN TRANSACTION;
 CREATE TABLE IF NOT EXISTS "user" (
 	"user_id"	INTEGER,
-	"name"	TEXT,
+	"name"	NUMERIC,
 	"temp_threshold"	INTEGER,
 	"humidity_threshold"	INTEGER,
 	"light_threshold"	INTEGER,
+	"picture"	TEXT,
 	PRIMARY KEY("user_id")
 );
 COMMIT;
